ffa284de8553f1e6089d9667fe03a446254959b4
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-05-18 16:17:25 +0530

    18.5

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../components/Login/LOgin'
+
+const LoginPage = () => {
+  return (
+    <div>
+        <Login></Login>
+    </div>
+  )
+}
+
+export default Login
